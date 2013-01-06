9d697f67667cd3219d8050726523c60ebaa9fb18
Florian Margaine <florian@margaine.com>
2013-01-06 16:34:15 +0100

    Changing the structure and getting closer to something treating data.

--- src/disassembler.lisp
+++ src/disassembler.lisp
@@ -2,9 +2,19 @@
 
 (in-package #:intel-8086-emulator)
 
+(defparameter *instructions* '#())
+
 (defun main ()
-  (with-open-file (stream "quicklisp/local-projects/intel-8086-emulator/asm/codegolf"
-                          :element-type 'unsigned-byte)
-    (loop for byte = (read-byte stream nil :eof)
-       until (eq byte :eof)
-       do (format t "~2X " byte))))
+  (let ((bytes '()))
+    (with-open-file (stream
+                     *asm-file*
+                     :element-type  'unsigned-byte)
+      (loop for byte = (read-byte stream nil :eof)
+         until (eq byte :eof)
+         do (setf bytes (append (format t "~2X " byte) bytes))))
+    bytes))
+
+(defclass instruction ()
+  ((address :accessor address :initarg :address)
+   (opcode :accessor opcode :initarg :opcode)
+   (operands :accessor operands :initarg :operands)))
--- src/intel-8086-emulator.lisp
+++ src/intel-8086-emulator.lisp
@@ -1,3 +1,5 @@
 ;; Hacks and glory awaits!
 
 (in-package #:intel-8086-emulator)
+
+(defvar *asm-file* "~/quicklisp/local-projects/intel-8086-emulator/asm/codegolf")
