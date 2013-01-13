77d64410cf99bb7541b9f5bde0adc3e0f64d00e3
Florian Margaine <florian@margaine.com>
2013-01-13 15:06:52 +0100

    Cleaner disassembling

--- src/disassembler.lisp
+++ src/disassembler.lisp
@@ -3,12 +3,13 @@
 (in-package #:intel-8086-emulator)
 
 (defun ndisasm ()
-  (let ((bytes '()))
-    (with-open-file (stream
-                     *asm-file*
-                     :element-type  'unsigned-byte)
-      (loop for byte = (read-byte stream nil :eof)
-         until (eq byte :eof)
-         do (setf bytes (append (list byte) bytes))))
-    (setf bytes (reverse bytes))
+  (let ((bytes (collect)))
     (instruction (first bytes) bytes)))
+
+(defun collect ()
+  (with-open-file (stream
+                   *asm-file*
+                   :element-type 'unsigned-byte)
+    (loop for byte = (read-byte stream nil :eof)
+         until (eq byte :eof)
+         collect byte)))
