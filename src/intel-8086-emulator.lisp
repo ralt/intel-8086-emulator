08658346e83ceb33cee216aba81c10f10b17db47
Florian Margaine <florian@margaine.com>
2013-01-27 14:54:06 +0100

    Refactoring so that opcides.lisp stays clean.

--- src/intel-8086-emulator.lisp
+++ src/intel-8086-emulator.lisp
@@ -3,3 +3,20 @@
 (in-package #:intel-8086-emulator)
 
 (defvar *asm-file* "~/quicklisp/local-projects/intel-8086-emulator/asm/codegolf")
+
+;;; Instruction implements an opcode and its bytes
+(defgeneric instruction (opcode bytes)
+  (:documentation "Defines an opcode with its bytes"))
+
+(defmacro defopcode (byte (opcode bytes) &body body)
+  "Defines an opcode with its byte."
+  `(defmethod instruction ((,opcode (eql ,byte)) ,bytes)
+     ,@body))
+
+;;; Called after every instruction.
+(defun next (bytes)
+  (instruction (first (rest bytes)) (rest bytes)))
+
+;;; Last method called.
+(defmethod instruction (opcode bytes)
+  (format t "END"))
--- src/opcodes.lisp
+++ src/opcodes.lisp
@@ -4,19 +4,7 @@
 
 (in-package #:intel-8086-emulator)
 
-;;; Instruction implements an opcode and its bytes
-(defgeneric instruction (opcode bytes)
-  (:documentation "Defines an opcode with its bytes"))
-
-(defmacro defopcode (byte (opcode bytes) &body body)
-  "Defines an opcode with its byte."
-  `(defmethod instruction ((,opcode (eql ,byte)) ,bytes)
-     ,@body))
-
-;;; Called after every instruction.
-(defun next (bytes)
-  (instruction (first (rest bytes)) (rest bytes)))
-
+;;; All opcodes starting with #x81
 (defopcode #x81 (opcode bytes)
   (instruction #x81fc bytes))
 
@@ -24,7 +12,3 @@
 (defopcode #x81fc (opcode bytes)
   (format t "cmp~%")
   (next bytes))
-
-;;; Last method called.
-(defmethod instruction (opcode bytes)
-  (format t "END"))
